43084d102e146a46092a2c58699909628ae15db7
zjxiaobai <897665728@qq.com>
2018-06-05 17:51:26 +0800

    modify to split data create

--- contro_env.py
+++ contro_env.py
@@ -168,131 +168,26 @@ max = step_size
 ground_height = 0.0
 
 
-
 env = gym.make('FetchPickAndPlace-v0')
 
-stage_set = ["raise_gasket_above_again"
 
-             "reach_object_above", "reach_object", "grasp_object", "raise_object_above",
-             "reach_gasket_above", "reach_gasket_half_above", "release", 
-
-             "reach_gasket_above_for_grasp", 
-
-             "reach_gasket", "grasp_gasket_and_object", "raise_gasket_and_object_above",
-             "reach_target_above", "reach_target_half_above", "release_gasket_and_object",
-
-             "check", "stay"]
-
-data = []
-trajectory_num = 0
-
-image_num_already_success = 0
+def make_trajectory(index):
 
 
-while True:
-    # begins a new trajectory
+    data = []
+    trajectory_num = 0
 
-    stage = "reach_object_above"
-    stage_outside = "step_1"
+    image_num_already_success = 0
 
-    observation = env.reset()
-    done = False
 
-    gripper_position = observation["my_new_observation"][0:3]
-    object_0_position = observation["my_new_observation"][5:8]
-    object_1_position = observation["my_new_observation"][8:11]
-    bow_0_position = observation["my_new_observation"][11:14]
-    bow_1_position = observation["my_new_observation"][14:17]
-    goal_0_position = observation["my_new_observation"][17:20]
-    goal_1_position = observation["my_new_observation"][20:23]
+    while True:
+        # begins a new trajectory
 
-    plus = 2
-    minus = 1
-    fixed = 0
-    hand_open = 1
-    close = 0
+        stage = "reach_object_above"
+        stage_outside = "step_1"
 
-    ground_height = object_0_position[2]
-
-    one_trajectory = []
-    final_success = False
-
-    image_num = image_num_already_success
-
-    strategy = np.random.permutation(4)
-
-    # to avoid all black image
-    # env.render(mode='rgb_array')
-
-    while not done:
-
-        # saving image
-        # image = env.render(mode='rgb_array')
-        # image = Image.fromarray(image)
-        # w, h = image.size
-        # image = image.resize((w//4, h//4),Image.ANTIALIAS)  
-        # image.save('images/'+ str(image_num) +'.jpg', 'jpeg')
-        # image_num += 1
-
-        # NOT saving image
-        # env.render()
-
-        if stage_outside == "step_1":
-            ob, tar = subtask_decide(strategy[0], 
-                                    object_0_position, object_1_position, 
-                                    bow_0_position, bow_1_position, 
-                                    goal_0_position, goal_1_position)
-            action_category, success = pick_and_place(gripper_position, ob, tar)
-            if success:
-                stage_outside = "step_2"
-                stage = "reach_object_above"
-        elif stage_outside == "step_2":
-            ob, tar = subtask_decide(strategy[1], 
-                                    object_0_position, object_1_position, 
-                                    bow_0_position, bow_1_position, 
-                                    goal_0_position, goal_1_position)
-            action_category, success = pick_and_place(gripper_position, ob, tar)
-            if success:
-                stage_outside = "step_3"
-                stage = "reach_object_above"
-        elif stage_outside == "step_3":
-            ob, tar = subtask_decide(strategy[2], 
-                                    object_0_position, object_1_position, 
-                                    bow_0_position, bow_1_position, 
-                                    goal_0_position, goal_1_position)
-            action_category, success = pick_and_place(gripper_position, ob, tar)
-            if success:
-                stage_outside = "step_4"
-                stage = "reach_object_above"
-        elif stage_outside == "step_4":
-            ob, tar = subtask_decide(strategy[3], 
-                                    object_0_position, object_1_position, 
-                                    bow_0_position, bow_1_position, 
-                                    goal_0_position, goal_1_position)
-            action_category, success = pick_and_place(gripper_position, ob, tar)
-            if success:
-                stage_outside = "outside_finish"
-                stage = "reach_object_above"
-                final_success = True
-
-        action = np.zeros((4))
-
-        # change from category to value
-        for i in range(3):
-            if action_category[i] == plus:
-                action[i] = (step_size/0.03)
-            elif action_category[i] == minus:
-                action[i] = -(step_size/0.03)
-            elif action_category[i] == fixed:
-                action[i] = 0.0
-
-        if action_category[3] == hand_open:
-            action[3] = 1.0
-        elif action_category[3] == close:
-            action[3] = -1.0
-
-        previous_observation = observation
-        observation, reward, done, info = env.step(action)
+        observation = env.reset()
+        done = False
 
         gripper_position = observation["my_new_observation"][0:3]
         object_0_position = observation["my_new_observation"][5:8]
@@ -301,29 +196,129 @@ while True:
         bow_1_position = observation["my_new_observation"][14:17]
         goal_0_position = observation["my_new_observation"][17:20]
         goal_1_position = observation["my_new_observation"][20:23]
-        
-        one_step = []
-        one_step.extend(previous_observation["my_new_observation"])
-        one_step.extend(action_category)
-        one_step.extend(observation["my_new_observation"])
-        one_step.extend([float(final_success)])
-        one_trajectory.append(one_step)
 
-        if stage_outside == "outside_finish":
-            data.extend(one_trajectory)
-            trajectory_num += 1
-            image_num_already_success += len(one_trajectory)
+        plus = 2
+        minus = 1
+        fixed = 0
+        hand_open = 1
+        close = 0
+
+        ground_height = object_0_position[2]
+
+        one_trajectory = []
+        final_success = False
+
+        image_num = image_num_already_success
+
+        strategy = np.random.permutation(4)
+
+        # to avoid all black image
+        # env.render(mode='rgb_array')
+
+        while not done:
+
+            # saving image
+            # image = env.render(mode='rgb_array')
+            # image = Image.fromarray(image)
+            # w, h = image.size
+            # image = image.resize((w//4, h//4),Image.ANTIALIAS)  
+            # image.save('images/'+ str(image_num) +'.jpg', 'jpeg')
+            # image_num += 1
+
+            # NOT saving image
+            # env.render()
+
+            if stage_outside == "step_1":
+                ob, tar = subtask_decide(strategy[0], 
+                                        object_0_position, object_1_position, 
+                                        bow_0_position, bow_1_position, 
+                                        goal_0_position, goal_1_position)
+                action_category, success = pick_and_place(gripper_position, ob, tar)
+                if success:
+                    stage_outside = "step_2"
+                    stage = "reach_object_above"
+            elif stage_outside == "step_2":
+                ob, tar = subtask_decide(strategy[1], 
+                                        object_0_position, object_1_position, 
+                                        bow_0_position, bow_1_position, 
+                                        goal_0_position, goal_1_position)
+                action_category, success = pick_and_place(gripper_position, ob, tar)
+                if success:
+                    stage_outside = "step_3"
+                    stage = "reach_object_above"
+            elif stage_outside == "step_3":
+                ob, tar = subtask_decide(strategy[2], 
+                                        object_0_position, object_1_position, 
+                                        bow_0_position, bow_1_position, 
+                                        goal_0_position, goal_1_position)
+                action_category, success = pick_and_place(gripper_position, ob, tar)
+                if success:
+                    stage_outside = "step_4"
+                    stage = "reach_object_above"
+            elif stage_outside == "step_4":
+                ob, tar = subtask_decide(strategy[3], 
+                                        object_0_position, object_1_position, 
+                                        bow_0_position, bow_1_position, 
+                                        goal_0_position, goal_1_position)
+                action_category, success = pick_and_place(gripper_position, ob, tar)
+                if success:
+                    stage_outside = "outside_finish"
+                    stage = "reach_object_above"
+                    final_success = True
+
+            action = np.zeros((4))
+
+            # change from category to value
+            for i in range(3):
+                if action_category[i] == plus:
+                    action[i] = (step_size/0.03)
+                elif action_category[i] == minus:
+                    action[i] = -(step_size/0.03)
+                elif action_category[i] == fixed:
+                    action[i] = 0.0
+
+            if action_category[3] == hand_open:
+                action[3] = 1.0
+            elif action_category[3] == close:
+                action[3] = -1.0
+
+            previous_observation = observation
+            observation, reward, done, info = env.step(action)
+
+            gripper_position = observation["my_new_observation"][0:3]
+            object_0_position = observation["my_new_observation"][5:8]
+            object_1_position = observation["my_new_observation"][8:11]
+            bow_0_position = observation["my_new_observation"][11:14]
+            bow_1_position = observation["my_new_observation"][14:17]
+            goal_0_position = observation["my_new_observation"][17:20]
+            goal_1_position = observation["my_new_observation"][20:23]
+            
+            one_step = []
+            one_step.extend(previous_observation["my_new_observation"])
+            one_step.extend(action_category)
+            one_step.extend(observation["my_new_observation"])
+            one_step.extend([float(final_success)])
+            one_trajectory.append(one_step)
+
+            if stage_outside == "outside_finish":
+                data.extend(one_trajectory)
+                trajectory_num += 1
+                image_num_already_success += len(one_trajectory)
+                break
+
+
+        print(trajectory_num)
+
+        if trajectory_num == 2400:
             break
 
+    print("total trajectory num is : ",end="")
+    print(image_num_already_success)
 
-    print(trajectory_num)
+    data = np.array(data)
+    pickle.dump(data, open("PP-24-paths-"+str(trajectory_num)+"-"+tra(index)+".p", "wb"))
 
-    if trajectory_num == 24000:
-        break
 
-print("total trajectory num is : ",end="")
-print(image_num_already_success)
-
-data = np.array(data)
-pickle.dump(data, open("PP-24-paths-"+str(trajectory_num)+".p", "wb"))
+for i in range(10):
+    make_trajectory(i)
 
